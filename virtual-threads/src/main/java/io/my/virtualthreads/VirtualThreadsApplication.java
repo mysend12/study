368b72f62ea3b25e71da82fa8ca254cf1b6e727d
mysend12 <mysend12@naver.com>
2023-02-02 08:48:34 +0900

    가상 스레드 jmeter 테스트용 api

---
+++ virtual-threads/src/main/java/io/my/virtualthreads/VirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package io.my.virtualthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualThreadsApplication.class, args);
+    }
+
+}
