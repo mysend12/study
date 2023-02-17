8ec68425c238e300cfd5ec8ad2dcede6faa319fe
mysend12 <mysend12@naver.com>
2023-02-17 19:42:49 +0900

    meta annotation 설정

--- toby-spring/tobyhelloboot/src/test/java/io/my/tobyhelloboot/HelloServiceTest.java
+++ toby-spring/tobyhelloboot/src/test/java/io/my/tobyhelloboot/HelloServiceTest.java
@@ -2,16 +2,37 @@ package io.my.tobyhelloboot;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Test
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@interface UnitTest { }
+
+@UnitTest
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@interface FastUnitTest { }
 
 class HelloServiceTest {
 
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         HelloService helloService = new SimpleHelloService();
         String result = helloService.sayHello("Test");
-
         assertEquals("Hello Test", result);
+    }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+        String hello = decorator.sayHello("Test");
+        assertEquals("*Test*", hello);
     }
 
 }
