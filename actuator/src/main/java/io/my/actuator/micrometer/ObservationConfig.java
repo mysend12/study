acbea58bfd3f85e0e6abff78924699476aea9352
mysend12 <mysend12@naver.com>
2023-03-13 21:55:13 +0900

    atlas 연동

--- actuator/src/main/java/io/my/actuator/micrometer/ObservationConfig.java
+++ actuator/src/main/java/io/my/actuator/micrometer/ObservationConfig.java
@@ -1,13 +1,35 @@
 package io.my.actuator.micrometer;
 
+import com.netflix.servo.publish.BasicMetricFilter;
+import com.netflix.servo.publish.MonitorRegistryMetricPoller;
+import com.netflix.servo.publish.PollRunnable;
+import com.netflix.servo.publish.atlas.AtlasMetricObserver;
+import com.netflix.servo.publish.atlas.BasicAtlasConfig;
+import com.netflix.servo.tag.BasicTagList;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class ObservationConfig {
+
+    @Bean
+    public AtlasMetricObserver atlasMetricObserver() {
+        System.setProperty("servo.pollers", "1000");
+        System.setProperty("servo.atlas.batchSize", "1");
+        System.setProperty("servo.atlas.uri", "http://localhost:7101/api/v1/publish");
+        AtlasMetricObserver observer = new AtlasMetricObserver(
+                new BasicAtlasConfig(), BasicTagList.of("servo", "counter"));
+
+        PollRunnable task = new PollRunnable(
+                new MonitorRegistryMetricPoller(), new BasicMetricFilter(true), observer);
+
+        return observer;
+    }
 
     @Bean
     public ObservationRegistry observationRegistry() {
@@ -21,6 +43,8 @@ public class ObservationConfig {
 
     @Bean
     public RestTemplate restTemplate() {
+        ObservationRegistry observationRegistry = observationRegistry();
+
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setObservationRegistry(observationRegistry());
         return restTemplate;
